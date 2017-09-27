5b8c40c7a1f350f7c15f2314f860c42b6aed38a9
alki22 <alki.collias@gmail.com>
2017-09-26 23:32:36 -0300

    Optimized memory usage and commented code

--- clustering.py
+++ clustering.py
@@ -9,32 +9,38 @@ from sklearn.externals import joblib
 from collections import defaultdict, OrderedDict
 
 parser = spacy.load('es')
-file_name = "xaa"
+
+# dataset/file to cluster
+file_name = "lavoztextodump.txt"
+
 with open(file_name, 'r') as file:
     text = file.read()
+
 parsedData = parser(text)
 stopwords = nltk.corpus.stopwords.words('spanish')
 
 corpus = []
+# (Vector number <-> word) dictionary
 vect_to_string = {}
 
-i = 0
+index = 0
 for token in parsedData:
-    if token.orth_.isalpha() and token.orth_ not in stopwords:
+    # Clean the corpus out of stopwords and non alphabetic tokens
+    if token.orth_.isalpha() and token.lower_ not in stopwords:
         token_features = {
                         'lemma' : token.lemma, 
                         'log probability' : token.prob, 'POS tag' : token.pos,
                         'dependency' : token.dep, 'head' : token.head.text,
                         'name': token.orth 
-                        }
+                        }                
         corpus.append(token_features)
-        vect_to_string[i] = token.orth_
-        i += 1
-
+        vect_to_string[index] = token.orth_
+        index += 1
 vectorizer = DictVectorizer()
 vectors = vectorizer.fit_transform(corpus)
+
 # Run K-Means algorithm
-k_clusters = 10
+k_clusters = 30
 km = KMeans(n_clusters=k_clusters, init='k-means++', n_jobs=-1)
 X = km.fit(vectors)
 
@@ -42,18 +48,14 @@ X = km.fit(vectors)
 joblib.dump(km,  'doc_cluster.pkl')
 clusters = defaultdict(list)
 
+# Put every word's index into its cluster
 for j in range(len(vect_to_string)):
-    clusters[X.labels_[j]].append(vect_to_string[j])
+    clusters[X.labels_[j]].append(j)
 
-sorted(clusters)
-
-# Print results
-for j in range(len(clusters)):
+# Print results to output.txt
+for j in range(len(clusters)):  
     print('Cluster', j, file=open("output.txt", "a"))
-    
-    # Remove duplicated entries
-    clusters[j] = list(OrderedDict.fromkeys(clusters[j]))
-    
-    for word in clusters[j]:
-            print(word, file=open("output.txt", "a"))
+    print('==========================', file=open("output.txt", "a"))
+    for k in clusters[j]:
+            print(vect_to_string[k], file=open("output.txt", "a"))
 
